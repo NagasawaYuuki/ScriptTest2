6b1dd18e472e0f2fa09aacf37fff160ad7e7a0fe
NagasawaYuuki <69589388+NagasawaYuuki@users.noreply.github.com>
2020-09-06 16:49:51 +0900

    初めてのコミット

---
+++ Assets/Test.cs
@@ -0,0 +1,36 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int[] array= new int[5];
+
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+
+        for(int i = 0;i<5;i++)
+        { 
+          Debug.Log(array[i]);  
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
